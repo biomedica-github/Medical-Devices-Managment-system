f5f0ac6044102ab8e546f2e96f98698399b6c4e2
edgar modesto <edgarmodesto23@gmail.com>
2023-07-10 23:31:55 -0700

    First_commit

---
+++ inventario/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+
+
+class Proveedor(models.Model):
+    nombre_proveedor = models.CharField(max_length=100)
+    contacto = models.CharField(max_length=100, null=True)
+
+class Contrato(models.Model):
+    CONTRATO_CONSOLIDADO = 'C'
+    CONTRATO_GARANTIA = 'G'
+    CONTRATO_LOCAL = 'L'
+    CONTRATO_OPCIONES = [
+        (CONTRATO_LOCAL, 'Contrato local'),
+        (CONTRATO_GARANTIA, 'Contrato garantia'),
+        (CONTRATO_CONSOLIDADO, 'Contrato consolidado')
+    ]
+    SERVICIO_PREVENTIVO = 'PRV'
+    SERVICIO_PREV_CORR = 'P/C'
+    NO_SERVICIO = 'N/A'
+    SERVICIO_OPCIONES = [
+        (SERVICIO_PREV_CORR, 'Servicio preventivo y correctivo'),
+        (SERVICIO_PREVENTIVO, 'Servicio  preventivo'),
+        (NO_SERVICIO, 'Proveedor no brinda servicio')
+    ]
+
+    proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
+    tipo_contrato = models.CharField(max_length=1, choices=CONTRATO_OPCIONES, default=CONTRATO_LOCAL)
+    tipo_servicio_estipulado = models.CharField(max_length=3, choices=SERVICIO_OPCIONES, default=SERVICIO_PREVENTIVO)
+    fecha_vencimiento = models.DateField(auto_now=False, auto_now_add=False)
+    num_servicios = models.PositiveIntegerField()
+
+class Especialidad(models.Model):
+    nombre_especialidad = models.CharField(max_length=50)
+
+class Sala_hospital(models.Model):
+    numero_sala = models.PositiveIntegerField()
+    encargado = models.CharField(max_length=100)
+    nombre_sala = models.CharField(max_length=100)
+    especialidad = models.ForeignKey(Especialidad, on_delete=models.SET_NULL, null=True)
+
+class Cama(models.Model):
+    numero_cama = models.PositiveIntegerField(primary_key=True)
+    sala = models.ForeignKey(Sala_hospital, on_delete=models.PROTECT)
+
+class Equipo_medico(models.Model):
+    numero_nacional_inv = models.CharField(max_length= 100, primary_key=True)
+    nombre_equipo = models.CharField(max_length=100)
+    estado = models.CharField(max_length=100)
+    observaciones = models.CharField(max_length=255, null=True)
+    numero_serie = models.CharField(max_length=255, null=True)
+    marca = models.CharField(max_length=50)
+    modelo = models.CharField(max_length=50, null=True)
+    cama = models.OneToOneField(Cama, on_delete=models.SET_NULL, null= True)
+    contrato = models.ForeignKey(Contrato, on_delete=models.SET_NULL, null=True)
+
+class Fecha_servicio(models.Model):
+    contrato = models.ForeignKey(Contrato, on_delete=models.CASCADE)
+    equipo_medico = models.ForeignKey(Equipo_medico, on_delete=models.CASCADE)
+    fecha = models.DateField(auto_now=False, auto_now_add=False)
