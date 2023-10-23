effbb7a6b7ef5391dc51ded33e9a311b58323cd4
edgar modesto <edgarmodesto23@gmail.com>
2023-10-23 06:36:21 -0700

    Cambiado a base de datos en linea

--- UMAE_db/settings.py
+++ UMAE_db/settings.py
@@ -106,16 +106,7 @@ WSGI_APPLICATION = "UMAE_db.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME"),
-        "HOST": os.environ.get("DB_HOST"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
-        "PORT": os.environ.get("DB_PORT"),
-    }
-}
+DATABASES = {"default": dj_database_url.config(default=config("DATABASE_URL"))}
 
 
 # Password validation
