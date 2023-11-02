dca948e0df8dfa0d9928b6db3a131f79333498fa
Pedro Gutiérrez <PedroGutierrez08@outlook.com>
2023-11-02 00:19:29 -0700

    Configuracion para s3 bucket

--- UMAE_db/settings.py
+++ UMAE_db/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
     "debug_toolbar",
     "core",
     "django_cleanup.apps.CleanupConfig",
+    "storages",
 ]
 
 INTERNAL_IPS = [
@@ -204,4 +205,16 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 if config('DJANGO_PRODUCTION', default=False, cast=bool):
     from .setting_production import *
 
+S3_ENABLED = config('S3_ENABLED', cast=bool, default=False)
+
+if S3_ENABLED:
+    AWS_ACCESS_KEY_ID = config('BUCKETEER_AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = config('BUCKETEER_AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = config('BUCKETEER_BUCKET_NAME')
+    AWS_S3_REGION_NAME = config('BUCKETEER_AWS_REGION')
+    AWS_DEFAULT_ACL = None
+    AWS_S3_SIGNATURE_VERSION = config('S3_SIGNATURE_VERSION', default='s3v4')
+    AWS_S3_ENDPOINT_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
--- inventario/pdf_generator.py
+++ inventario/pdf_generator.py
@@ -1,6 +1,8 @@
 from fpdf import FPDF
 from fpdf.fonts import FontFace
 
+import boto3
+import os
 
 class PDF(FPDF):
     def header(self):
@@ -207,5 +209,9 @@ def generarOrdenServicio(orden_de_servicio):
     pdf.output(
         name="media/Ordenes_servicio/" + f"OrdenServicio_{id}_{equipo_medico}.pdf"
     )
+
+    s3 = boto3.client('s3')
+    with open(f"OrdenServicio_{id}_{equipo_medico}.pdf", "rb") as f:
+        s3.upload_fileobj(f, "bucketeer-74a9acd7-e668-4ac9-9fdd-de748191c984", "media/Ordenes_servicio/" + f"OrdenServicio_{id}_{equipo_medico}.pdf")
 
     return "Ordenes_servicio/" + f"OrdenServicio_{id}_{equipo_medico}.pdf"
